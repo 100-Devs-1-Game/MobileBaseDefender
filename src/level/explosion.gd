7b9d9bfb07eb93e9c51a917cb4dde053870664f4
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-12-03 15:35:21 +0100

    Replace explosion effect

--- src/level/explosion.gd
+++ src/level/explosion.gd
@@ -4,6 +4,9 @@ extends Node2D
 var damage: float
 var radius: float
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
 
 func init(pos: Vector2, dmg: float, r: float):
 	position= pos
@@ -11,9 +14,14 @@ func init(pos: Vector2, dmg: float, r: float):
 	radius= r
 
 
-func _draw() -> void:
-	var color:= Color.ORANGE
-	color.a= 0.5
-	draw_circle(Vector2.ZERO, radius, color)
-	var tween:= create_tween()
-	tween.tween_property(self, "modulate", Color.TRANSPARENT, 0.4)
+func _ready() -> void:
+	animated_sprite.scale= Vector2.ONE * radius / 128.0
+
+#
+#
+#func _draw() -> void:
+	#var color:= Color.ORANGE
+	#color.a= 0.5
+	#draw_circle(Vector2.ZERO, radius, color)
+	#var tween:= create_tween()
+	#tween.tween_property(self, "modulate", Color.TRANSPARENT, 0.4)
