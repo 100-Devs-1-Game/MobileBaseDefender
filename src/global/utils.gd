ab7ccbff983715420784efcffc17d564287a2d4d
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-12-04 23:13:45 +0100

    Add chance100()

--- src/global/utils.gd
+++ src/global/utils.gd
@@ -1,6 +1,11 @@
 class_name Utils
 
 
+
+static func chance100(c: float)-> bool:
+	return randf() * 100 < c
+
+
 static func remove_children(node: Node):
 	for child in node.get_children():
 		node.remove_child(child)
