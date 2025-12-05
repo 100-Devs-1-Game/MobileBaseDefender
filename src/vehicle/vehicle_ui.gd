17792914cc5373ecc7590c910c193af9cb77f3cd
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-12-05 16:50:31 +0100

    Toggeable minimap

--- src/vehicle/vehicle_ui.gd
+++ src/vehicle/vehicle_ui.gd
@@ -1,6 +1,8 @@
 class_name VehicleUI
 extends CanvasLayer
 
+@export var enable_minimap: bool= true
+
 @onready var vehicle: Vehicle= get_parent()
 @onready var texture_minimap: TextureRect = %"Texture Minimap"
 @onready var dynamic_minimap_overlay: Control = %"Dynamic Minimap Overlay"
@@ -13,15 +15,25 @@ func _ready() -> void:
 	assert(vehicle)
 	await vehicle.ready
 	
-	minimap_data= vehicle.get_level().minimap_data
-	texture_minimap.texture= minimap_data.texture
+	if enable_minimap:
+		minimap_data= vehicle.get_level().minimap_data
+		if not minimap_data:
+			enable_minimap= false
+		
+	if enable_minimap:
+		texture_minimap.texture= minimap_data.texture
 
 
 func _physics_process(_delta: float) -> void:
+	if not enable_minimap:
+		return
 	dynamic_minimap_overlay.queue_redraw()
 
 
 func _on_dynamic_minimap_overlay_redraw(canvas_item: CanvasItem):
+	if not enable_minimap:
+		return
+
 	var vehicle_pos:= minimap_data.get_local_pos(vehicle.position)
 	var trans:= vehicle.global_transform
 	var points: PackedVector2Array
@@ -34,6 +46,8 @@ func _on_dynamic_minimap_overlay_redraw(canvas_item: CanvasItem):
 
 
 func _on_static_minimap_overlay_on_draw(canvas_item: CanvasItem) -> void:
+	if not enable_minimap:
+		return
 	var rect: Rect2
 	rect.position= minimap_data.get_local_pos(minimap_data.target.position)
 	rect.end= minimap_data.get_local_pos(minimap_data.target.end)
