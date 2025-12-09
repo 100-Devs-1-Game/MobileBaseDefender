52cc9cc6749379a4048b46e61b14e663dfb5a0b1
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-12-09 03:17:55 +0100

    Add solar panel

---
+++ src/vehicle/parts/custom/solar_panel_part_data.gd
@@ -0,0 +1,24 @@
+class_name VehicleSolarPanelPartData
+extends VehicleMountedPartData
+
+const SUN_DATA= "sun"
+
+@export var power_generated: int
+
+var query:= PhysicsPointQueryParameters2D.new()
+
+
+
+func init(part_info: VehicleMountedPartInfo, _vehicle: Vehicle):
+	query.collision_mask= CollisionLayers.CLOUDS
+	part_info.live_data[SUN_DATA]= false
+
+
+func tick(part_info: VehicleMountedPartInfo, vehicle: Vehicle, tile_pos: Vector2i, _delta: float): 
+	query.position= vehicle.get_tile_transform(tile_pos).origin
+	var result= vehicle.get_world_2d().direct_space_state.intersect_point(query, 1)
+	part_info.live_data[SUN_DATA]= result.is_empty()
+
+
+func get_power_production(part_info: VehicleMountedPartInfo)-> float:
+	return power_generated if part_info.live_data[SUN_DATA] else 0
