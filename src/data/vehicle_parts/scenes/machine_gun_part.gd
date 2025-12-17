33d3f8852dd6bd6cad71aa85600ab1a4dd4509f1
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-12-17 02:22:30 +0100

    Fix chaingun animation logic

--- src/data/vehicle_parts/scenes/machine_gun_part.gd
+++ src/data/vehicle_parts/scenes/machine_gun_part.gd
@@ -3,13 +3,21 @@ extends VehicleMountedPartObject
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
-func tick(_vehicle: Vehicle, _delta: float):
+func tick(vehicle: Vehicle, _delta: float):
 	var rot: float= part_info.live_data[VehicleRotatingGunPart.ROTATION_DATA]
 	animated_sprite.global_rotation= rot + PI / 2
 	
-	var has_target:= VehicleRotatingGunPart.has_target(part_info)
-	if animated_sprite.is_playing() and not has_target:
+	if animated_sprite.is_playing() and not can_shoot(vehicle):
 		animated_sprite.stop()
-	elif not animated_sprite.is_playing() and has_target:
+	elif not animated_sprite.is_playing() and can_shoot(vehicle):
 		animated_sprite.play("default")
+
+
+func can_shoot(vehicle: Vehicle)-> bool:
+	var has_target:= VehicleRotatingGunPart.has_target(part_info)
+	if not has_target:
+		return false
+	if not vehicle.fire_groups[FireGroup.Type.AIMING].active:
+		return false
+	return true
 	
