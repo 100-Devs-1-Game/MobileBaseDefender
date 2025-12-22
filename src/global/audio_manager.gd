087107fe3f005f5f57eacc6197da8bb57ed469b7
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-12-22 22:26:26 +0100

    Reduce frequency of audio player distance sorting

--- src/global/audio_manager.gd
+++ src/global/audio_manager.gd
@@ -11,13 +11,15 @@ func register_efficient_player(player: AudioStreamPlayerEfficient):
 
 
 func _physics_process(_delta: float) -> void:
-	efficient_players.sort_custom(sort_players_by_distance)
-	var ctr:= 0
-	for player in efficient_players:
-		player.stream_paused= ctr > MAX_ACTIVE_PLAYERS
-		ctr+= 1
+	if Engine.get_physics_frames() % 60 == 0:
+		efficient_players.sort_custom(sort_players_by_distance)
+		var ctr:= 0
+		for player in efficient_players:
+			player.stream_paused= ctr > MAX_ACTIVE_PLAYERS
+			ctr+= 1
 
 
 func sort_players_by_distance(a: AudioStreamPlayerEfficient, b: AudioStreamPlayerEfficient):
-	var camera: Camera2D= Global.level.get_viewport().get_camera_2d()
-	return a.global_position.distance_to(camera.global_position) < b.global_position.distance_to(camera.global_position)
+	var target_pos: Vector2= Global.vehicle.position
+	return a.global_position.distance_squared_to(target_pos) < \
+		b.global_position.distance_squared_to(target_pos)
